9b4c7eb4c9de7c2cfa8ad4ff3e46dcedf2fbfa79
lancao2 <lancao.af@gmail.com>
2022-04-18 18:45:11 -0300

    feat: Congratutetion page without style

--- src/pages/congratulation/index.jsx
+++ src/pages/congratulation/index.jsx
@@ -1,11 +1,20 @@
 import { CongratsStyled } from "./style";
 import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Congratutations = () => {
   const { user } = useParams();
   return (
     <CongratsStyled>
-      <h1>{user}</h1>
+      <div className="conteiner">
+        <div className="msg">
+          <h1>Seja muito bem vindo!</h1>
+          <h2>{user}</h2>
+        </div>
+        <Link to="/">
+          <button>Voltar</button>
+        </Link>
+      </div>
     </CongratsStyled>
   );
 };
